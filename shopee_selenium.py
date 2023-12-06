0022df50515ea44ddc0198b7bf4ef3854e5e1d63
MinJeLi <mj.yesturnkey@gmail.com>
2023-12-07 00:24:23 +0800

    1. add handle shadow-root with shopee pop-up banner 2. print pop-up banner image src

--- shopee_selenium.py
+++ shopee_selenium.py
@@ -13,8 +13,8 @@ class shopeeSelenium(unittest.TestCase):
     def test_home_banner_in_shopee(self):
         
         driver = self.driver
-        self.home_banner = driver.find_element(By.CLASS_NAME,'full-home-banners-wrapper')
-        self.assertNotEqual(self.home_banner,None)
+        home_banner = driver.find_element(By.CLASS_NAME,'full-home-banners-wrapper')
+        self.assertNotEqual(home_banner,None)
         
     def test_elements_below_home_banner(self):
         
@@ -26,6 +26,22 @@ class shopeeSelenium(unittest.TestCase):
         for ele in below_home_banner:
             href_value = ele.get_attribute("href")
             print(href_value)
+            
+    def test_simple_banner(self):
+        
+        driver =self.driver
+        # find shadow-root host
+        shopee_main = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div/div/shopee-banner-popup-stateful')
+        
+        # shadow-root
+        first_shadow_root = shopee_main.shadow_root
+        home_content = first_shadow_root.find_element(By.CSS_SELECTOR, 'div > div > div > shopee-banner-simple')
+        
+        # shadow-root again
+        second_shadow_root = home_content.shadow_root
+        target_element = second_shadow_root.find_element(By.CSS_SELECTOR, 'div > a > img')
+        img_src = target_element.get_attribute("src")
+        print(img_src)
             
     
     def tearDown(self):
